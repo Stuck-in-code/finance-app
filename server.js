7fa4392d05945b253eb65cbe8bea9b0e4373ddd9
stuck in code <bonidaniel52@gmail.com>
2026-04-14 16:09:28 +0000

    cron job added

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import transactionRouter from "./routes/transactionsRoutes.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 const app = express();
 
+if(process.env.NODE_ENV === "production") job.start()
+
 app.use(express.json());
 app.use("/api/transaction", transactionRouter);
 
+app.get("/api/health", (req,res) =>{
+  res.status(200).json({status: "ok"})
+})
 
 app.get("/", (req, res) => {
   res.status(200).send("my app is working alright");
